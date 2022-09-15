cd6ab90a4c12ac553641bebd73952a03e99ba442
jyt0106 <107200165+jyt0106@users.noreply.github.com>
2022-09-15 20:21:05 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Base64;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,6 +44,7 @@ import com.github.tvbox.osc.bean.SourceBean;
 import com.github.tvbox.osc.bean.VodInfo;
 import com.github.tvbox.osc.cache.CacheManager;
 import com.github.tvbox.osc.event.RefreshEvent;
+import com.github.tvbox.osc.player.MyVideoView;
 import com.github.tvbox.osc.player.controller.VodController;
 import com.github.tvbox.osc.player.thirdparty.MXPlayer;
 import com.github.tvbox.osc.player.thirdparty.ReexPlayer;
@@ -86,10 +88,10 @@ import java.util.concurrent.Executors;
 
 import me.jessyan.autosize.AutoSize;
 import xyz.doikki.videoplayer.player.ProgressManager;
-import xyz.doikki.videoplayer.player.VideoView;
+
 
 public class PlayActivity extends BaseActivity {
-    private VideoView mVideoView;
+   private MyVideoView mVideoView;
     private TextView mPlayLoadTip;
     private ImageView mPlayLoadErr;
     private ProgressBar mPlayLoading;
@@ -246,8 +248,14 @@ public class PlayActivity extends BaseActivity {
                 stopParse();
                 if (mVideoView != null) {
                     mVideoView.release();
+                       String zimuParamKey = "___zimu___"; //字幕url的header中key
+                    String zimuBase64Url = "";
                     if (url != null) {
                         try {
+                                        if (headers != null && headers.containsKey(zimuParamKey)) {
+                                zimuBase64Url = headers.get(zimuParamKey);
+                                headers.remove(zimuParamKey);//remove传过来的字幕header的key
+                            }
                             int playerType = mVodPlayerCfg.getInt("pl");
                             // takagen99: Check for External Player
                             extPlay = false;
@@ -282,12 +290,25 @@ public class PlayActivity extends BaseActivity {
                         hideTip();
                         PlayerHelper.updateCfg(mVideoView, mVodPlayerCfg);
                         mVideoView.setProgressKey(progressKey);
+                          String zimuUrl = "";
+                        if (zimuBase64Url != null && zimuBase64Url.length() > 0) {
+                            zimuUrl = new String(Base64.decode(zimuBase64Url, Base64.DEFAULT));
+                            mController.mSubtitleView.setVisibility(View.GONE);
+                        }
                         if (headers != null) {
                             mVideoView.setUrl(url, headers);
                         } else {
                             mVideoView.setUrl(url);
+                            
                         }
                         mVideoView.start();
+                         if (zimuUrl != null && zimuUrl .length() > 0) {
+                            // 绑定MediaPlayer
+                            mController.mSubtitleView.bindToMediaPlayer(mVideoView.getMediaPlayer());
+                            // 设置字幕
+                            mController.mSubtitleView.setSubtitlePath(zimuUrl);
+                            mController.mSubtitleView.setVisibility(View.VISIBLE);
+                        }
                         mController.resetSpeed();
                     }
                 }
